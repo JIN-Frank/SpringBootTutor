4ad76a7d7737421dcf094930edf3f872f55c27dc
JIN-Frank <q1jin@ucsd.edu>
2019-05-26 17:48:05 -0700

    xx

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +13,14 @@ import java.util.List;
 @RestController
 @RequestMapping
 public class CourseController {
+    @Autowired  // IOC 控制反转
+    CourseService courseService;  // singleton
 
-    @GetMapping(path = "/", produces = "application/json")
+    @GetMapping(path = "/", produces = "application/json")   //如果你用get方法 call我这个地址
     public HttpEntity findAllCourses(){
-       // List<CourseDto> allCourses = courseService.findAllCourses();
+        List<Course> allCourses = courseService.findAllCourses();  // 我就帮你用这个方法处理这个请求
 
-        return new ResponseEntity<>("OK",HttpStatus.OK);
+        return new ResponseEntity<>(allCourses,HttpStatus.OK);  // 返回结果给你
     }
 }
+
